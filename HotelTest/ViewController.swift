f55bf816b800190bd0e7cae3218100b6c7c16e16
Alexander <nefest71@gmail.com>
2023-09-04 16:13:19 +0300

    Initial commit

---
+++ HotelTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HotelTest
+//
+//  Created by Alexander V. on 04.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
